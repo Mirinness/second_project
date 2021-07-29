f13cf87908ef3fd4bf648804540cf5cda09168a1
illia <marketesh@gmail.com>
2021-07-29 15:07:54 +0300

    lab_3_5_2

--- lab_3_5/lab_3_5.py
+++ lab_3_5/lab_3_5.py
@@ -6,4 +6,27 @@ arr3 = [char for char in string.digits]
 arr+=arr2+arr3
 print(arr)
 
+'''Print 1st symbol of list'''
+print(arr[0])
+
+'''Print last symbol'''
+print(arr[-1])
+
+'''Print 3rd from start and 3rd from the end'''
+print(arr[2])
+print(arr[-3])
+
+'''Slice first 10 symbols'''
+print(arr[0:10])
+
+'''Print only symbols with index, which divides on 2 without remaining'''
+print(arr[::2])
+
+'''Print only integer values from list'''
+for i in range(len(arr)):
+    if type(arr[i])==type(int):
+        print(arr[i])
+
+'''Reverse list using slice'''
+print(arr[::-1])
 
