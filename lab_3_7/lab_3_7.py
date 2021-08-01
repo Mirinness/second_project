804505621919123ca199bf6c7a6b49e09fa8a7a7
illia <marketesh@gmail.com>
2021-08-01 18:47:55 +0300

    lab_3_7

---
+++ lab_3_7/lab_3_7.py
@@ -0,0 +1,29 @@
+'''Generate two sets with not unique numbers and few symbols'''
+a = set('fish164')
+b = set('crab154')
+
+'''Print 1st set'''
+print(a)
+
+'''Create tuple from intersection of first and second set'''
+t = tuple(a) + tuple(b)
+
+'''Create tuple from difference first and second set'''
+t2 = a-b
+print(t2)
+
+'''Slice first 3 symbols from first tuple'''
+print(t[:3])
+
+'''Print only symbols with numbers from second set''' '''----------ПЕРЕВІРИТИ----------'''
+for i in b:
+    if type(i) == int:
+        print(b[i])
+        print(' ok ')
+
+'''Reverse tuple using slice'''
+print(t[::-1])
+
+'''Convert both tuples into list'''
+l = list(t) + list(t2)
+print(l)
